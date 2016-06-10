e8657635a03a8b3cacbae512e31539bd48a3259c
Maxim Savencov <msavencov@gmail.com>
2016-06-10 08:26:08 +0300

    restored generation of keys

--- Academy.BattleShip.Entity/Migrations/201606100511013_Current.cs
+++ Academy.BattleShip.Entity/Migrations/201606100511013_Current.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Academy.BattleShip.Entity.Migrations
 {
     using System;
@@ -51,7 +54,20 @@ namespace Academy.BattleShip.Entity.Migrations
                         Key = c.String(nullable: false, maxLength: 7),
                     })
                 .PrimaryKey(t => t.Key);
-            
+
+            var items = from x in Enumerable.Range(10, 90) from y in Enumerable.Range(100, 900) select +y + "-" + x;
+            var lines = new List<string>(items);
+
+            IEnumerable<string> iteration;
+            var current = 0;
+
+            while ((iteration = lines.Skip(current).Take(1000)).Any())
+            {
+                var values = iteration.Aggregate(string.Empty, (current1, s) => current1 + (", " + "('" + s + "')")).TrimStart(' ', ',');
+                Sql("insert into KeyPools([Key]) values " + values);
+                current += 1000;
+            }
+
         }
         
         public override void Down()
